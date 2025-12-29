0fa7fe86926eacbfe360cedcf633d7fc696c3283
Mahendra <mahendrathungha.99@gmail.com.com>
2025-12-29 13:34:44 +0530

    Added certificates in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import isoLogo from '../assets/ISO Badge...jpg';
+import iesaLogo from '../assets/ChatGPT Image Dec 29, 2025, 12_45_05 PM.png';
 
 function Home() {
   return (
-    <div className="home">
-      <div className="overlay-text">
-        <h1>With decades of experience in electronics, we’re fueling the software-driven future of global industries.</h1>
-        <p><strong>Your success starts here — explore our solutions and services.</strong></p>
-      </div>
-    </div>
+    <>
+      {/* HERO SECTION */}
+      <section className="home">
+        <div className="overlay-text">
+          <h1>
+            With decades of experience in electronics, we’re fueling the
+            software-driven future of global industries.
+          </h1>
+          <p>
+            <strong>
+              Your success starts here — explore our solutions and services.
+            </strong>
+          </p>
+        </div>
+      </section>
+
+      <section className="certifications-section">
+        <div className="cert-card">
+          <img src={isoLogo} alt="ISO 9001:2015 Certified" />
+          <h3 className="iso-text">ISO 9001:2015 Certified</h3>
+        </div>
+
+        <div className="cert-card">
+          {/* <img src={iesaLogo} alt="IESA Member" className='iesa-img' /> */}
+          <img src={iesaLogo} alt="IESA Member" />
+          <h3 className="iesa-text">IESA Member</h3>
+        </div>
+      </section>
+    </>
   );
 }
 
